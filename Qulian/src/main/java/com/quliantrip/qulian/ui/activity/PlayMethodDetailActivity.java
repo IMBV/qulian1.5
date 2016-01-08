67c99ae9fe9f705dbdcecbfe43702c25d16b9689
yuly <yuliangyang@quliantrip.com>
2016-01-08 17:47:21 +0800

    节点01

--- Qulian/src/main/java/com/quliantrip/qulian/ui/activity/PlayMethodDetailActivity.java
+++ Qulian/src/main/java/com/quliantrip/qulian/ui/activity/PlayMethodDetailActivity.java
@@ -1,9 +1,24 @@
 package com.quliantrip.qulian.ui.activity;
 
+import android.content.Context;
+import android.os.Bundle;
+
+import com.quliantrip.qulian.R;
+
+import butterknife.ButterKnife;
 import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
 
 /**
  * Created by Yuly on 2016/1/8.
  */
 public class PlayMethodDetailActivity extends SwipeBackActivity {
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_play_method_detail);
+        ButterKnife.bind(this);
+        mContext = this;
+    }
 }
