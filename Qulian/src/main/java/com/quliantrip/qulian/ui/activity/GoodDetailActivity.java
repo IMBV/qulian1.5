25816c63f1740ab2fd2979bba37b610c3f539c01
yuly <yuliangyang@quliantrip.com>
2016-01-08 18:03:41 +0800

    界面调整

--- Qulian/src/main/java/com/quliantrip/qulian/ui/activity/GoodDetailActivity.java
+++ Qulian/src/main/java/com/quliantrip/qulian/ui/activity/GoodDetailActivity.java
@@ -32,12 +32,6 @@ import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
  */
 public class GoodDetailActivity extends SwipeBackActivity {
     private Context mContext;
-    @Bind(R.id.wv_good_activity)
-    RelativeLayout activity;
-    @Bind(R.id.wv_good_detail)
-    WebView mGoodDetail;
-    @Bind(R.id.pb_good_list_loading)
-    FrameLayout loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,89 +39,7 @@ public class GoodDetailActivity extends SwipeBackActivity {
         setContentView(R.layout.fragment_good_detail);
         ButterKnife.bind(this);
         mContext = this;
-        initDate();
     }
 
-    //加载布局进行显示数据
-    private void initDate() {
-        mGoodDetail.addJavascriptInterface(new QulianInterface(), "qulianInterface");
 
-        mGoodDetail.loadUrl(getIntent().getStringExtra("goodId"));
-
-//        mGoodDetail.loadUrl("http://dev.qulian.com/wap/index.php?ctl=uc_ecv&act=exchange");
-
-        mGoodDetail.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
-
-        mGoodDetail.getSettings().setJavaScriptEnabled(true);
-        mGoodDetail.getSettings().setAllowFileAccess(true);
-        mGoodDetail.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-
-        mGoodDetail.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                if ("finish".equals(getIntent().getStringExtra("isFinish"))) {
-                    mGoodDetail.loadUrl("javascript:androidloginout()");
-                    finish();
-                }
-                if (getIntent().getStringExtra("goodId").toString().endsWith("ctl=user&act=login")) {
-                    String content = "android";
-                    mGoodDetail.loadUrl("javascript:javacalljswithargs('" + content + "')");
-                }
-                if(getIntent().getStringExtra("goodId").toString().endsWith("?ctl=user&act=getpassword")){
-                    String content = "android";
-                    mGoodDetail.loadUrl("javascript:javacalljswithargs('" + content + "')");
-                }
-                loading.setVisibility(View.GONE);
-                activity.setVisibility(View.VISIBLE);
-                super.onPageFinished(view, url);
-            }
-
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                super.onPageStarted(view, url, favicon);
-            }
-        });
-
-        mGoodDetail.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
-                AlertDialog.Builder b2 = new AlertDialog.Builder(GoodDetailActivity.this)
-                        .setTitle("提示信息").setMessage(message)
-                        .setPositiveButton("确定", new AlertDialog.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                result.confirm();
-                            }
-                        });
-                b2.setCancelable(false);
-                b2.create();
-                b2.show();
-                return true;
-            }
-        });
-    }
-
-    /**
-     * Js调用的JavascriptInterface
-     */
-    public class QulianInterface {
-
-        @JavascriptInterface
-        public void startFunction() {
-            Intent intent = new Intent(mContext, MainActivity.class);
-            setResult(RESULT_OK, intent);
-            finish();
-        }
-
-        @JavascriptInterface
-        public void tipsInfo(String arg) {
-//            ToastUtil.showToast(mContext, arg);
-        }
-    }
-    @OnClick(R.id.tv_good_back) void finishGoogDetail(){
-        finish();
-    }
-    @OnClick(R.id.tv_good_menu) void showMenu(){
-        ToastUtil.showToast(mContext,"该功能尚未开通，敬请期待");
-    }
 }
