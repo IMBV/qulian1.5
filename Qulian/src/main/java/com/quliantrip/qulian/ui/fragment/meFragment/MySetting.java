d52ba84b89fd1c10f3c162988147ce51d995a828
yuly <yuliangyang@quliantrip.com>
2016-01-09 11:38:55 +0800

    项目瘦身

--- Qulian/src/main/java/com/quliantrip/qulian/ui/fragment/meFragment/MySetting.java
+++ Qulian/src/main/java/com/quliantrip/qulian/ui/fragment/meFragment/MySetting.java
@@ -1,7 +1,12 @@
 package com.quliantrip.qulian.ui.fragment.meFragment;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.text.format.Time;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.quliantrip.qulian.R;
@@ -9,6 +14,8 @@ import com.quliantrip.qulian.base.BaseFragment;
 import com.quliantrip.qulian.global.QulianApplication;
 import com.quliantrip.qulian.ui.activity.SimpleBackActivity;
 import com.quliantrip.qulian.util.ToastUtil;
+
+import java.util.Date;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -39,6 +46,7 @@ public class MySetting extends BaseFragment {
 //        QulianApplication.getInstance().cleanLoginInfo();
 //        ((SimpleBackActivity)mContext).finish();
 //    }
+
 //    //修改密码
 //    @OnClick(R.id.tv_my_setting_updatePaw) void updataPaw(){
 //        ToastUtil.showToast(QulianApplication.getContext(),"暂时还没有实现");
@@ -49,7 +57,11 @@ public class MySetting extends BaseFragment {
     void downNumber() {
         Integer newNumber = new Integer(number.getText().toString().trim());
         newNumber = newNumber - 1;
-        number.setText(newNumber + "");
+        if (newNumber > 0) {
+            number.setText(newNumber + "");
+        } else {
+            ToastUtil.showToast(mContext, "人数不能少于0");
+        }
     }
 
     @OnClick(R.id.iv_good_person_number_add)
@@ -57,5 +69,56 @@ public class MySetting extends BaseFragment {
         Integer newNumber = new Integer(number.getText().toString().trim());
         newNumber = newNumber + 1;
         number.setText(newNumber + "");
+    }
+
+    @Bind(R.id.tv_preview_time_data_text)
+    TextView pretime;
+
+    @OnClick(R.id.rl_preview_time_data_setting)
+    void setPriviewTime() {
+        Time time = new Time();
+        time.setToNow();
+        int year = time.year;
+        int month = time.month;
+        int day = time.monthDay;
+        DatePickerDialog datePicker = new DatePickerDialog(mContext, new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                pretime.setText(year + " - " + (monthOfYear + 1) + " - " + dayOfMonth);
+            }
+        }, year, month, day);
+        datePicker.show();
+    }
+
+    @Bind(R.id.tv_serve_time_text)
+    TextView serveTime;
+
+    @OnClick(R.id.rl_serve_time_setting)
+    void setServeTime() {
+        Time time = new Time();
+        time.setToNow();
+        int minute = time.minute;
+        int hour = time.hour;
+
+        TimePickerDialog timeDialog = new TimePickerDialog(mContext, new TimePickerDialog.OnTimeSetListener() {
+
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                if(minute < 10){
+                    serveTime.setText(hourOfDay + " : 0" + minute);
+                }else{
+                    serveTime.setText(hourOfDay + " : " + minute);
+                }
+            }
+        }, hour, minute, true);
+        timeDialog.show();
+    }
+    @Bind(R.id.tv_check_store_result_text)
+    TextView checkSrore;
+    @OnClick(R.id.rl_check_store_setting)
+    void checkStore(){
+        ToastUtil.showToast(mContext,"请选择门店");
     }
 }
