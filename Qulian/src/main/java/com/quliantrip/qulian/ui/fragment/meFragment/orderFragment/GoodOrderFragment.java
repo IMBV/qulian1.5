e85268837605f3426b59425c41d934c961ab2fe9
yuly <yuliangyang@quliantrip.com>
2016-01-13 20:00:56 +0800

    界面调整

--- Qulian/src/main/java/com/quliantrip/qulian/ui/fragment/meFragment/orderFragment/GoodOrderFragment.java
+++ Qulian/src/main/java/com/quliantrip/qulian/ui/fragment/meFragment/orderFragment/GoodOrderFragment.java
@@ -6,7 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.quliantrip.qulian.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Qulian5 on 2016/1/5.
@@ -14,7 +20,8 @@ import android.widget.TextView;
 public class GoodOrderFragment extends Fragment {
     private Context mContext;
     private View view;
-
+    @Bind(R.id.ll_line)
+    LinearLayout line;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,8 +32,9 @@ public class GoodOrderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(mContext);
-        textView.setText("玩法订单");
-        return textView;
+        view = View.inflate(mContext, R.layout.fragment_me_see_consumer, null);
+        ButterKnife.bind(this, view);
+        line.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        return view;
     }
 }
